fixed all the matrix multiplications that were facing the wrong way and updated the indices that were off by one

main.py
@@ -5,85 +5,107 @@ from array import array
 from os.path  import join
 from matplotlib import pyplot as plt
 
+
+
 class Network:
-    def __init__(self, *layer_sizes, loss='squared error', hidden_activation='relu', output_activation='softmax'):
+    def __init__(self, *layer_sizes):
         self.layer_sizes = layer_sizes
         self.layer_count = len(self.layer_sizes)
 
-        accepted_loss_types = ['squared error']
-        if not loss in accepted_loss_types:
-            raise ValueError(f"{loss} is not an accepted loss type. The accepted values are {accepted_loss_types}")
-        self.loss_type = loss
-
-        accepted_activation_types = ['relu', 'softmax']
-
-        if not hidden_activation in accepted_activation_types:
-            raise ValueError(f"{hidden_activation} is not an accepted hidden activation function. The accepted values are {accepted_activation_types}")
-        self.hidden_activation = hidden_activation
-
-        if not output_activation in accepted_activation_types:
-            raise ValueError(f"{output_activation} is not an accepted output activation function. The accepted values are {accepted_activation_types}")
-        self.output_activation = output_activation
-        
         self.weights = []
         self.weight_gradients = []
 
         self.biases = []
         self.bias_gradients = []
 
+        # self.activations[index] are the inputs to the weights of weights[index]
         self.activations = [np.zeros(self.layer_sizes[0])] # The first index is the input to the network
         self.errors = []
-        
+
         for i in range(self.layer_count - 1):
-            self.weights.append(np.random.rand(self.layer_sizes[i + 1], self.layer_sizes[i]) * 2 - 1)
+            self.weights.append(np.random.rand(
+                self.layer_sizes[i + 1],
+                self.layer_sizes[i])
+            - 0.5)
             self.weight_gradients.append(np.zeros(self.weights[i].shape))
 
             self.biases.append(np.zeros(self.layer_sizes[i + 1]))
             self.bias_gradients.append(np.zeros(self.biases[i].shape))
 
             self.activations.append(np.zeros(self.layer_sizes[i + 1]))
-            self.errors.append(np.zeros(self.layer_sizes[i + 1]))
-
+    
     def feed_forward(self, x):
         # If the input shape doesn't match the size of the first layer
         if len(x) != self.layer_sizes[0]:
             raise ValueError(f'Expected inputs of size {self.layer_sizes[0]}. Got {len(x)}')
-        
+
         self.activations[0] = x
         # 1 is subtracted because the first 'layer' is just the input to the network and doesn't have any processing
         for i in range(self.layer_count - 1):
-            z = self.weights[i] @ self.activations[i] + self.biases[i]
-            activation_type = self.output_activation if i == (self.layer_count - 2) else self.hidden_activation # If it's the last layer use the output activation type
-            if activation_type == 'relu':
-                activation = np.maximum(0, z)
-            elif activation_type == 'softmax':
-                exponents = np.exp(z - np.max(z))
-                activation = exponents / np.sum(exponents)
+            z = (self.weights[i] @ self.activations[i]) + self.biases[i]
+            # If we're on the output layer
+            if i == self.layer_count - 2:
+                activation = self.softmax(z)
+            else:
+                activation = self.relu(z)
+
             self.activations[i + 1] = activation
-        
+
         return self.activations[-1]
-    
-    def back_propagate(self, x, learning_rate):
+
+    # This is mostly based off of chapter 2 in Neural Network and Deep Learning: http://neuralnetworksanddeeplearning.com/chap2.html
+    def back_propagate(self, x, y, learning_rate):
         self.feed_forward(x)
 
-        errors = []
-        # Error from loss to last layer activation
-        if self.loss_type == 'squared error':
-            loss_grad = 2 * self.activations[-1]
-        
-        # Error from the last layer activation to z
-        if self.
+        errors = [np.zeros(layer_size) for layer_size in self.layer_sizes[1:]]
+ 
+        # You don't need to calculate the softmax derivative, because when combined with the
+        # cross entropy loss it the error simplifies
+        errors[-1] = self.activations[-1] - y
+        self.weights[-1] -= learning_rate * (errors[-1].reshape(-1, 1) @ self.activations[-2].reshape(1, -1))
+        self.biases[-1] -= learning_rate * errors[-1]
+
+        # Now for all the other layers
+
+        for i in reversed(range(self.layer_count - 2)): # Minus two because the last layer is already figured out
+            errors[i] = (self.weights[i + 1].T @ errors[i + 1]) * self.relu_grad(self.activations[i + 1])
+            self.weights[i] -= learning_rate * (errors[i].reshape(-1, 1) @ self.activations[i].reshape(1, -1))
+            self.biases[i] -= learning_rate * errors[i].flatten()
 
 
-# Following region is from https://www.kaggle.com/code/hojjatk/read-mnist-dataset?scriptVersionId=9466282&cellId=1
-# region MnistDataLoader    
-#
-# This is a sample Notebook to demonstrate how to read "MNIST Dataset"
-#
 
-#
-# MNIST Data Loader Class
-#
+    def squared_error_loss(self, x, y):
+        return np.sum((y - x) ** 2) / len(x)
+
+    def squared_error_grad(self, x):
+        return (2 * x) / len(x)
+
+    
+    def cross_entropy_loss(self, x, y, epsilon=1e-10):
+        x = np.clip(x, epsilon, 1.0 - epsilon)
+        return -np.sum(y * np.log(x))
+
+    def cross_entropy_grad(self, x, y):
+        return x - y
+
+    def softmax(self, z):
+        exponents = np.exp(z - np.max(z))
+        return exponents / np.sum(exponents)
+
+    def softmax_grad(self, x):
+        # Code from here: https://stackoverflow.com/questions/40575841/numpy-calculate-the-derivative-of-the-softmax-function/40576872#40576872
+        SM = np.reshape(x, newshape=(-1, 1))
+        return np.diagflat(x) - (SM @ SM.T)
+
+    def relu(self, z):
+        return np.maximum(0, z)
+
+    def relu_grad(self, x):
+        return x > 0
+
+
+
+# Following class is from https://www.kaggle.com/code/hojjatk/read-mnist-dataset?scriptVersionId=9466282&cellId=1
 class MnistDataloader(object):
     def __init__(self, training_images_filepath,training_labels_filepath,
                  test_images_filepath, test_labels_filepath):
@@ -91,68 +113,52 @@ class MnistDataloader(object):
         self.training_labels_filepath = training_labels_filepath
         self.test_images_filepath = test_images_filepath
         self.test_labels_filepath = test_labels_filepath
-    
-    def read_images_labels(self, images_filepath, labels_filepath):        
+
+    def read_images_labels(self, images_filepath, labels_filepath):
         labels = []
         with open(labels_filepath, 'rb') as file:
             magic, size = struct.unpack(">II", file.read(8))
             if magic != 2049:
                 raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
-            labels = array("B", file.read())        
-        
+            labels = array("B", file.read())
+
         with open(images_filepath, 'rb') as file:
             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
             if magic != 2051:
                 raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
-            image_data = array("B", file.read())        
+            image_data = array("B", file.read())
         images = []
         for i in range(size):
             images.append([0] * rows * cols)
         for i in range(size):
             img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
             img = img.reshape(28, 28)
-            images[i][:] = img            
-        
+            images[i][:] = img
+
         return images, labels
-            
+
     def load_data(self):
         x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
         x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
-        return (x_train, y_train),(x_test, y_test)        
-
-
-# endregion
+        return (x_train, y_train),(x_test, y_test)
 
 input_path = 'data'
 training_images_filepath = join(input_path, 'train-images-idx3-ubyte/train-images-idx3-ubyte')
 training_labels_filepath = join(input_path, 'train-labels-idx1-ubyte/train-labels-idx1-ubyte')
 test_images_filepath = join(input_path, 't10k-images-idx3-ubyte/t10k-images-idx3-ubyte')
-test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')    
+test_labels_filepath = join(input_path, 't10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte')
 
 mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
 
 (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
 
 x_train = [np.array(x).flatten() for x in x_train]
-y_train = [np.array(y).flatten() for y in y_train]
+y_train = np.eye(10)[y_train]
 
 x_test = [np.array(x).flatten() for x in x_test]
-y_test = [np.array(y).flatten() for y in y_test]
+y_test = np.eye(10)[y_test]
 
 the_network = Network(28 * 28, 16, 16, 10)
-output = the_network.feed_forward(x_train[0])
-plt.bar(range(len(output)), output)
-output = the_network.feed_forward(x_train[1])
-plt.bar(range(10), output)
-plt.show()
-print(the_network.feed_forward(x_train[0]))
-# Some pseudocode
-"""
-x1 = ReLU((input * W1) + b1)
-x2 = ReLU((x1 * W2) + b2)
-x3 = ReLU((x2 * W3) + b3)
-loss = (x3 - example)
-
-d(x3) = (((x2 * W3) + b3) > 0) * W3 * d(x2)
-d(x2) = (((x1 * W2) + b2) > 0) * W2 * d(x1)
-"""
+print(the_network.cross_entropy_loss(the_network.feed_forward(x_train[0]), y_train[0]))
+the_network.back_propagate(x_train[0], y_train[0], 0.1)
+print(the_network.cross_entropy_loss(the_network.feed_forward(x_train[0]), y_train[0]))